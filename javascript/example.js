c9399bea1b15495a08f2cf4ee9f491bfed50ab68
pranitha74 <bpranitha_cse230576@mgit.ac.in>
2025-08-19 14:19:34 +0530

    Create example.js

---
+++ javascript/example.js
@@ -0,0 +1,8 @@
+// JavaScript example: Function to reverse a string
+
+function reverseString(str) {
+    return str.split('').reverse().join('');
+}
+
+// Test the function
+console.log(reverseString("GitHub")); // Output: buHtiG
